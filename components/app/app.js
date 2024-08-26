c948cd7b1341f1f6325c21d06fdf812a9e72a80b
lopendres2 <antonmenshikov70@gmail.com>
2024-08-26 03:35:56 +0300

    'new'

--- components/app/app.js
+++ components/app/app.js
@@ -5,39 +5,92 @@ tg.expand();
 tg.MainButton.textColor = '#FFFFFF';
 tg.MainButton.color = '#2cab37';
 
-let selectedItem = "";
+let item = "";
 
-// Функция для обработки нажатия на кнопки
-function handleButtonClick(button, itemId) {
-    if (tg.MainButton.isVisible) {
-        tg.MainButton.hide();
-        selectedItem = "";
-    } else {
-        tg.MainButton.setText(`Вы выбрали товар ${itemId}`);
-        selectedItem = itemId;
-        tg.MainButton.show();
-    }
-}
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let btn5 = document.getElementById("btn5");
+let btn6 = document.getElementById("btn6");
 
-// Навешиваем обработчики на все кнопки
-document.querySelectorAll('.item-btn').forEach((button, index) => {
-    button.addEventListener('click', () => handleButtonClick(button, index + 1));
+btn1.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 1!");
+		item = "1";
+		tg.MainButton.show();
+	}
 });
 
-// Отправка данных при нажатии на главную кнопку Telegram
-Telegram.WebApp.onEvent("mainButtonClicked", function() {
-    tg.sendData(selectedItem);
+btn2.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 2!");
+		item = "2";
+		tg.MainButton.show();
+	}
 });
 
-// Отображение информации о пользователе
+btn3.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 3!");
+		item = "3";
+		tg.MainButton.show();
+	}
+});
+
+btn4.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 4!");
+		item = "4";
+		tg.MainButton.show();
+	}
+});
+
+btn5.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 5!");
+		item = "5";
+		tg.MainButton.show();
+	}
+});
+
+btn6.addEventListener("click", function(){
+	if (tg.MainButton.isVisible) {
+		tg.MainButton.hide();
+	}
+	else {
+		tg.MainButton.setText("Вы выбрали товар 6!");
+		item = "6";
+		tg.MainButton.show();
+	}
+});
+
+
+Telegram.WebApp.onEvent("mainButtonClicked", function(){
+	tg.sendData(item);
+});
+
+
 let usercard = document.getElementById("usercard");
 
-if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
-    let firstName = tg.initDataUnsafe.user.first_name || "";
-    let lastName = tg.initDataUnsafe.user.last_name || "";
-    let p = document.createElement("p");
-    p.innerText = `${firstName} ${lastName}`.trim();
-    usercard.appendChild(p);
-} else {
-    console.error("Telegram user data is not available.");
-}
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
